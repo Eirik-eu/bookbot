bbdf09a996b8662612fd71e08f104f2c3e1bb448
Eirik-eu <github.com.earthworm256@passmail.com>
2025-07-27 15:32:45 +0200

    refactored main to have a dedicated print_report function

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 from stats import count_words, count_characters, sort_list
 
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = count_words(text)
+    num_chars = count_characters(text)
+    print_report(book_path, num_words, num_chars)
+    
+
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
     
-def main():
-    book_text = get_book_text('books/frankenstein.txt')
+    
+def print_report(book_path, num_words, num_chars):
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at books/frankenstein.txt...")
     print("----------- Word Count ----------")
-    print(f"Found {count_words(book_text)} total words")
+    print(f"Found {num_words} total words")
     print("--------- Character Count -------")
     # for loop to only print alphanumerical characters
-    for item in sort_list(count_characters(book_text)):
+    for item in sort_list(num_chars):
         if item["char"].isalpha():
             print(f"{item["char"]}: {item["num"]}")
 
