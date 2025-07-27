dc1c77d2c0b03acb8f19ed8f4e45b766ae85d081
Eirik-eu <github.com.earthworm256@passmail.com>
2025-07-27 15:18:33 +0200

    added sorted output of most common letters

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_characters
+from stats import count_words, count_characters, sort_list
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -6,7 +6,14 @@ def get_book_text(filepath):
     
 def main():
     book_text = get_book_text('books/frankenstein.txt')
-    print(f"{count_words(book_text)} words found in the document.")
-    print(count_characters(book_text))
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {count_words(book_text)} total words")
+    print("--------- Character Count -------")
+    # for loop to only print alphanumerical characters
+    for item in sort_list(count_characters(book_text)):
+        if item["char"].isalpha():
+            print(f"{item["char"]}: {item["num"]}")
 
 main()
--- stats.py
+++ stats.py
@@ -9,5 +9,17 @@ def count_characters(text):
             characters_dict[character] += 1
         else:
             characters_dict[character] = 1
-
     return characters_dict
+
+# A function that takes a dictionary and returns the value of the "num" key
+# This is how the `.sort()` method knows how to sort the list of dictionaries
+def sort_on(items):
+    return items["num"]
+
+def sort_list(dict):
+    sorted_list = []
+    for item in dict:
+        sorted_list.append({"char": item, "num": dict[item]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+    
